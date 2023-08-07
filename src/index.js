6c512c3a01096e41e6e4d0143ef1c16464e3f95b
Fabian Ayala <fabianjoseayalavargas@gmail.com>
2023-08-07 14:51:58 -0400

    V1.1 Added Google Analytics, fixed description bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 import ScrollToTop from './Components/TemplateComp/ScrollToTop';
+
+ReactGA.initialize("G-YERLX2EJL6");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
